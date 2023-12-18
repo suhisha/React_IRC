f999d34ebcbeab125b75397eea825667f9343123
suhisha <suhishaj@gmail.com>
2023-12-18 15:30:09 +0530

    Myfile

--- CC1/myapp/src/Components/Home.js
+++ CC1/myapp/src/Components/Home.js
@@ -4,6 +4,41 @@ import {Link} from 'react-router-dom';
 function Home() {
     return (
       <div>
+
+<nav class="nav">
+<ul>
+  <li><a href="#" class="logo">
+    <img src="https://t3.ftcdn.net/jpg/03/04/90/30/360_F_304903020_Norp4P6o19mbV9p7oOeNPThiNnce3ZHH.jpg"/>
+    <span class="nav-item">Admin</span>
+  </a></li>
+  <li><a href="#">
+    <i class="fas fa-menorah"></i>
+    <span class="nav-item">Dashboard</span>
+  </a></li>
+  <li><a href="#">
+    <i class="fas fa-comment"></i>
+    <span class="nav-item">Message</span>
+  </a></li>
+  <li><a href="#">
+    <i class="fas fa-database"></i>
+    <span class="nav-item">Report</span>
+  </a></li>
+  <li><a href="#">
+    <i class="fas fa-chart-bar"></i>
+    <span class="nav-item">Attendance</span>
+  </a></li>
+  <li><a href="#">
+    <i class="fas fa-cog"></i>
+    <span class="nav-item">Setting</span>
+  </a></li>
+  <li><a href="#" class="logout">
+    <i class="fas fa-sign-out-alt"></i>
+    <span class="nav-item">Log out</span>
+  </a></li>
+</ul>
+</nav>
+
+
        <header>
           <h1>Toy Store</h1>
           <nav>
@@ -71,6 +106,7 @@ function Home() {
           <p>&copy; 2023 Toy Store</p>
       </footer>
       </div>
+
     );
   }
   
--- Day7/myapp/src/App.js
+++ Day7/myapp/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './Assets/Dashboard.css';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div class="container">
+      <div class="side-nav">
+          <div class="user">
+              <img src="https://i.ibb.co/LQc5grb/user.png" class="user-img"/>
+              <div>
+                  <h2>Farman Ali</h2>
+                  <p>@farmanali6349</p>
+              </div>
+              <img src="https://i.ibb.co/XxnbtTD/blue-tick.png" class="blue-tick"/>
+          </div>
 
+          <ul>
+              <li><img src="https://i.ibb.co/xKnDp3D/dashboard.png"/><p>Dashboard</p></li>
+              <li><img src="https://i.ibb.co/w0rKrFt/memebers.png"/><p>Members</p></li>
+              <li><img src="https://i.ibb.co/wN97xkd/messages.png"/><p>Messages</p></li>
+              <li><img src="https://i.ibb.co/QQ0wmsj/video-chat.png"/><p>Video Chat</p></li>
+              <li><img src="https://i.ibb.co/b7qLHwG/projects.png"/><p>Projects</p></li>
+              <li><img src="https://i.ibb.co/T1ngrm8/rewards.png"/><p>Rewards</p></li>
+              <li><img src="https://i.ibb.co/NWK9B0J/reports.png"/><p>Report</p></li>
+              <li><img src="https://i.ibb.co/fqrBhpH/setting.png"/><p>Setting</p></li>
+          </ul>
+
+          <ul>
+              <li><img src="https://i.ibb.co/nBhzgkh/logout.png"/><p>Logout</p></li>
+          </ul>
+      </div>
+  </div>
+    );
+}
 export default App;
